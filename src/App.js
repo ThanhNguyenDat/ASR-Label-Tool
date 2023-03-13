f2c02770e06ef042e965353cd57d967e4335f440
ThanhNguyenDat <johnkelvin3107@gmail.com>
2023-03-14 01:38:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Waveform from "./components/Waveform";
 
-const url_audio =
+const audioUrl =
   "https://api.twilio.com//2010-04-01/Accounts/AC25aa00521bfac6d667f13fec086072df/Recordings/RE6d44bc34911342ce03d6ad290b66580c.mp3";
 
 function App() {
+  const [shouldReplay, setShouldReplay] = useState(false);
+  const [options, setOptions] = useState({});
+
+  useEffect(() => {
+    setOptions({
+      autoPlay: shouldReplay,
+      loop: shouldReplay,
+    });
+  }, [shouldReplay]);
+
+  function handleToggleReplay() {
+    setShouldReplay((prevShouldReplay) => !prevShouldReplay);
+  }
+
   return (
     <div className="App">
-      <Waveform url_audio={url_audio} />
+      <Waveform audioUrl={audioUrl} options={options} />
+      <button onClick={handleToggleReplay}>
+        {shouldReplay ? "Disable Replay" : "Enable Replay"}
+      </button>
     </div>
   );
 }
--- src/components/Waveform/Waveform.jsx
+++ src/components/Waveform/Waveform.jsx
@@ -1,51 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
 import RegionsPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.min.js";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
-
-const wavesurferOptions = {
-  container: "#waveform",
-  waveColor: "violet",
-  progressColor: "purple",
-  plugins: [RegionsPlugin.create()],
-};
 
 function Waveform(props) {
+  const { audioUrl, options } = props;
+
   const [wavesurfer, setWavesurfer] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [autoReplay, setAutoReplay] = useState(false);
-
-  const [regions, setRegions] = useState([]);
-  const [startPosition, setStartPosition] = useState(null);
-  const [regionData, setRegionData] = useState({});
-  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const waveRef = useRef(null);
 
+  const defaultOptions = {};
+
   useEffect(() => {
-    const options = props.options || {};
+    const wavesurferOptions = options || defaultOptions;
     const wavesurferInstance = WaveSurfer.create({
       container: waveRef.current,
-      ...options,
+      plugins: [RegionsPlugin.create()],
+      ...wavesurferOptions,
     });
-    setWavesurfer(wavesurferInstance);
 
-    if (props.url_audio) {
-      wavesurferInstance.load(props.url_audio);
+    if (audioUrl) {
+      wavesurferInstance.load(audioUrl);
     }
+
+    setWavesurfer(wavesurferInstance);
+    setIsPlaying(false);
+
     return () => {
       wavesurferInstance.destroy();
     };
-  }, [props.url_audio, props.options]);
+  }, [audioUrl]);
 
   useEffect(() => {
-    if (wavesurfer && props.url_audio !== wavesurfer.getCurrentTime()) {
-      wavesurfer.load(props.url_audio);
-      setIsPlaying(false);
+    if (wavesurfer && isPlaying) {
+      wavesurfer.on("finish", () => {
+        if (!options?.autoPlay.value) setIsPlaying(false);
+      });
     }
-  }, [props.url_audio, wavesurfer]);
+  }, [isPlaying]);
 
   // Handle Play or Pause audio
   const handlePlayPause = () => {
@@ -60,49 +53,10 @@ function Waveform(props) {
     }
   };
 
-  // Handle Replay audio
-  const handleReplay = () => {
-    if (wavesurfer && autoReplay) {
-      console.log("1");
-      wavesurfer.seekTo(0);
-
-      wavesurfer.play();
-      setIsPlaying(true);
-    }
-  };
-
-  // Set enable or disable Replay button
-  const handleToggleReplay = () => {
-    setAutoReplay(!autoReplay);
-  };
-
-  useEffect(() => {
-    console.log("auto replay: ", autoReplay);
-    if (wavesurfer && autoReplay) {
-      wavesurfer.on("finish", handleReplay);
-    }
-  }, [wavesurfer, autoReplay]);
-
-  // annotaion regions
-  useEffect(() => {
-    if (wavesurfer) {
-      wavesurfer.on("region-click", (region) => {
-        setRegionData({
-          start: region.start,
-          end: region.end,
-          text: region.data.text,
-        });
-      });
-    }
-  }, []);
-
   return (
     <div>
       <div ref={waveRef}></div>
       <button onClick={handlePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
-      <button onClick={handleToggleReplay}>
-        {autoReplay ? "Disable Replay" : "Enable Replay"}
-      </button>
     </div>
   );
 }
