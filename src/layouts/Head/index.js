d58e60760a5ffcd3142d3ab1a636f345a06f6eb5
ThanhNguyenDat <johnkelvin3107@gmail.com>
2023-03-16 10:39:03 +0700

    add file useScript

---
+++ src/layouts/Head/index.js
@@ -0,0 +1,12 @@
+import useScript from "../../hooks/useScript";
+
+function Header() {
+  useScript("header");
+  return (
+    <div>
+      <head>Header</head>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/ASRAnnotaionPage/ASRAnnotaionPage.jsx
+++ src/pages/ASRAnnotaionPage/ASRAnnotaionPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Waveform from "../../components/Waveform";
+import useScript from "../../hooks/useScript";
 
 // const _audioUrl =
 //   "https://api.twilio.com//2010-04-01/Accounts/AC25aa00521bfac6d667f13fec086072df/Recordings/RE6d44bc34911342ce03d6ad290b66580c.mp3"; // bi loi
@@ -8,6 +9,8 @@ import Waveform from "../../components/Waveform";
 //   "https://assets.mixkit.co/active_storage/sfx/1714/1714-preview.mp3"; // khoong bi loi
 
 function ASRAnnotaionPage() {
+  useScript("https://label.lab.zalo.ai/ui/ailab_ui_api.js");
+
   const [options, setOptions] = useState({
     height: 100,
     pixelRatio: 1,
