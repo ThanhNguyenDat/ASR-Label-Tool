5f1c699a7f0e5627de7633210ff9e15ddf92ef60
ThanhNguyenDat <johnkelvin3107@gmail.com>
2023-06-27 01:20:54 +0700

    add css to ASRBenchmark

--- frontend/src/pages/projects/asr/asr_benchmark/ASRBenchmarkList.jsx
+++ frontend/src/pages/projects/asr/asr_benchmark/ASRBenchmarkList.jsx
@@ -15,7 +15,7 @@ import {
 } from 'react-admin';
 import PlayPauseButton from '../../../../components/buttons/PlayPauseButton';
 import ASRBenchmarkDatagridExpand from './ASRBenchmarkDatagridExpand';
-
+import './styles.scss';
 
 const fieldfilters = [
     <TextInput label="Search" source="q" alwaysOn />,
@@ -41,23 +41,24 @@ const ASRBenchmarkGoogleList = props => {
             actions={<ListActions />}
             sort={{ field: 'id', order: 'ASC' }}
             perPage={10}
+            className='asr-benchmark-list'
         >
             <DatagridConfigurable
                 // omit={['seed', 'full_text', 'wer_google', 'wer_kaldi', 'wer_wenet']}
                 omit={['id', 'label_url', 'predict_google', 'predict_kaldi', 'predict_wenet']}
                 expand={<ASRBenchmarkDatagridExpand />}
-                
+                className='asr-benchmark-datagrid'
             >
-                <TextField source="id"/>
-                <TextField source="seed" />
-                <TextField source="label_url" />
-                <TextField source="full_text" />
-                <TextField source="predict_google" />
-                <TextField source="wer_google" />
-                <TextField source="predict_kaldi" />
-                <TextField source="wer_kaldi" />
-                <TextField source="predict_wenet" />
-                <TextField source="wer_wenet" />
+                <TextField source="id" cellClassName='id'/>
+                <TextField source="seed" cellClassName='seed'/>
+                <TextField source="label_url" cellClassName='label_url'/>
+                <TextField source="full_text" cellClassName='full_text' />
+                <TextField source="predict_google" cellClassName='predict_google' />
+                <TextField source="wer_google" cellClassName='wer_google' />
+                <TextField source="predict_kaldi" cellClassName='predict_kaldi' />
+                <TextField source="wer_kaldi" cellClassName='wer_kaldi' />
+                <TextField source="predict_wenet" cellClassName='predict_wenet' />
+                <TextField source="wer_wenet" cellClassName='wer_wenet' />
                 <PlayPauseButton />
             </DatagridConfigurable>
         </List>
